c87407ba2b5f41843b1d5f612c1747305c180ce8
Alec S <asummers34@gmail.com>
2023-02-27 13:45:26 -0700

    added the homepage routes

--- server.js
+++ server.js
@@ -37,6 +37,14 @@ app.post("/api/notes", (req, res) => {
   res.status(201).json(db);
 });
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
